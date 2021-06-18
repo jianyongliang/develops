b6bdf6192a555025affe020b59b40fe7d0dba631
lieflat <865431557@qq.com>
2021-06-18 08:22:16 +0800

    Add files via upload

---
+++ gateway/src/main/java/com/anyuan/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.anyuan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author liangjy on 2021/4/28.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
